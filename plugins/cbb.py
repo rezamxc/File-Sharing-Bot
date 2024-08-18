180b873735fa91d55885e8e48b5fe8c1bf427b17
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-08-18 16:10:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Master : <a href='tg://user?id={OWNER_ID}'>Dalvir </a>\n○ Channel: <a href='https://t.me/HSTV_OFFICIAL'>@HSTv_Official</a>\n○ Support Group : @HSTv_Support</b>",
+            text = f"<b>○ Master : <a href='tg://user?id={OWNER_ID}'>Sachin </a>\n○ Channel: <a href='https://t.me/HSTV_OFFICIAL'>@Chennai_Movies</a>\n○ Support Group : @KPT_Help_bot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
